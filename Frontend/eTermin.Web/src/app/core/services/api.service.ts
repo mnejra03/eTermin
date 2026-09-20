a72379b508d93ea1439413e276a11d7176966805
Nejra Muminović <nejramuminovic0@gmail.com>
2026-09-20 20:46:57 +0200

    Setup Angular frontend

---
+++ Frontend/eTermin.Web/src/app/core/services/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private readonly apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  get<T>(endpoint: string) {
+    return this.http.get<T>(
+      `${this.apiUrl}/${endpoint}`
+    );
+  }
+
+  post<T>(endpoint: string, body: unknown) {
+    return this.http.post<T>(
+      `${this.apiUrl}/${endpoint}`,
+      body
+    );
+  }
+
+  put<T>(endpoint: string, body: unknown) {
+    return this.http.put<T>(
+      `${this.apiUrl}/${endpoint}`,
+      body
+    );
+  }
+
+  delete<T>(endpoint: string) {
+    return this.http.delete<T>(
+      `${this.apiUrl}/${endpoint}`
+    );
+  }
+}
---
+++ Frontend/eTermin.Web/src/app/features/salons/salons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salons',
+  imports: [],
+  templateUrl: './salons.component.html',
+  styleUrl: './salons.component.scss'
+})
+export class SalonsComponent {
+
+}
